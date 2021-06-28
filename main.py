bbfa1a67b1a164b364fe89414938ee6bfc52353a
carlortiz <mr.carlortiz@gmail.com>
2021-06-28 13:31:58 -0700

    User can now check a letter by pressing enter But figure out how to change image and phrase

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ BUTTONS_GAP_X = 80
 BUTTONS_GAP_Y = 65
 
 # game variables
-hangman_status = 0
 directions_y = 100
 button_x = 35
 button_y = 550
@@ -53,14 +52,19 @@ directions = ["Welcome to HANGMAN! Instructions below",
               "letter you have in mind is light blue,",
               "press 'enter' to check if it's correct.",
               "Press space to continue"]
+checked_letters = []
+
 letters = []
 A = 65
 for x in range(26):
     letter = chr(A + x)
     letters.append([letter, False, False])
 letters_length = len(letters)
-hangman_image = images[hangman_status]
-hangman_image = pygame.transform.scale(hangman_image, (300, 265))
+
+
+def set_initial_selected_letter():
+    first_letter = letters[0]
+    return first_letter
 
 
 def get_mystery_phrase(phrase):
@@ -93,29 +97,54 @@ def get_new_letter(change, chosen_letter):
     next_letter = letters.index(chosen_letter) + change
     if 0 <= next_letter < letters_length:
         next_letter = letters[next_letter]
-        chosen_letter[1] = False
         return next_letter
     else:
         return chosen_letter
 
 
-def check_spacebar(event):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
+def find_nearest_letter(chosen_letter):
+    distance = 1
+    while True:
+        next_letter = get_new_letter(distance, chosen_letter)
+        if next_letter[2]:
+            distance = distance * -1
+            if distance > 0:
+                distance += 1
+        else:
+            return next_letter
+
+
+def check_if_spacebar_pressed(event):
+    if event.key == pygame.K_SPACE:
             return True
     return False
 
 
+def check_if_enter_pressed(event, chosen_letter):
+    if event.type == pygame.K_RETURN:
+        chosen_letter[2] = True
+
+
 def check_for_movement(event, chosen_letter):
     if event.key == pygame.K_RIGHT:
+        chosen_letter[1] = False
         chosen_letter = get_new_letter(1, chosen_letter)
     elif event.key == pygame.K_LEFT:
+        chosen_letter[1] = False
         chosen_letter = get_new_letter(-1, chosen_letter)
     elif event.key == pygame.K_UP:
+        chosen_letter[1] = False
         chosen_letter = get_new_letter(-HALF_OF_ALPHABET, chosen_letter)
     elif event.key == pygame.K_DOWN:
+        chosen_letter[1] = False
         chosen_letter = get_new_letter(HALF_OF_ALPHABET, chosen_letter)
     return chosen_letter
+
+
+def get_letter_checked_status(letter):
+    if letter[2]:
+        return True
+    return False
 
 
 def draw_directions(directions_y):
@@ -163,22 +192,32 @@ def main():
     clock = pygame.time.Clock()
     run = True
     start_menu_status = True
-    first_letter = letters[0]
-    chosen_letter = first_letter
+    chosen_letter = set_initial_selected_letter()
+
+    hangman_status = 0
 
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if check_spacebar(event):
+            if check_if_spacebar_pressed(event):
                 start_menu_status = False
             if event.type == pygame.KEYDOWN:
                 chosen_letter = check_for_movement(event, chosen_letter)
+                check_if_enter_pressed(event, chosen_letter)
 
+
+        checked_letter = get_letter_checked_status(chosen_letter)
+        if checked_letter:
+            chosen_letter = find_nearest_letter(chosen_letter)
         if start_menu_status:
             draw_start_window()
             continue
+        # for letter in checked_letters:
+            # hangman_status += 1
+        hangman_image = images[hangman_status]
+        hangman_image = pygame.transform.scale(hangman_image, (300, 265))
         draw_game_window(hangman_image, chosen_letter)
     pygame.quit()
 
